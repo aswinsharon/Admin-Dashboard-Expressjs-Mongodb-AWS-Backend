9533a4535c148f50308da9b65c15e121dc040503
Aswin Sharon <aswinsharon@computer.local>
2023-12-04 21:14:06 +0530

    added validations

---
+++ src/config/constants.ts
@@ -0,0 +1,10 @@
+export abstract class Constants {
+  //<--- status codes --->
+  static readonly HTTP_OK_STATUS_CODE = 200;
+  static readonly HTTP_CREATED_STATUS_CODE = 201;
+  static readonly HTTP_SUCCESS_NO_CONTENT_STATUS_CODE = 204;
+  static readonly HTTP_BAD_REQUEST_STATUS_CODE = 400;
+  static readonly HTTP_UNAUTHORIZED_STATUS_CODE = 401;
+  static readonly HTTP_NOT_FOUND_STATUS_CODE = 404;
+  static readonly HTTP_SERVER_ERROR_STATUS_CODE = 500;
+}
